d79b4189275d496cd65d956bdb7a940cfe33d98f
lizavetion <alizafox2000@gmail.com>
2023-06-11 16:07:03 +0300

    made 1&2 tasks

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('[data-start]')
+const stopBtn = document.querySelector('[data-stop]');
+let timerId;
 
+stopBtn.disabled = true;
+
+const handlerOnStart = () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+const handlerOnStop = () => {
+  clearInterval(timerId);
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+}
+
+startBtn.addEventListener('click', handlerOnStart);
+stopBtn.addEventListener('click', handlerOnStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,72 @@
+import flatpickr from 'flatpickr';
+import "flatpickr/dist/flatpickr.min.css";
+
+const dateInputEl = document.querySelector('#datetime-picker');
+const startBtnEl = document.querySelector('[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secEl = document.querySelector('[data-seconds]');
+let selectedData;
+let timerId;
+
+startBtnEl.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    selectedDate = selectedDates[0];
+    if (selectedDate.getTime() > options.defaultDate.getTime()){
+      startBtnEl.disabled = false;
+    } else {
+      startBtnEl.disabled = true;
+      window.alert('Please choose a future date')
+    }
+  }
+};
+
+const fp = flatpickr(dateInputEl, options);
+
+const handlerOnStart = () => {
+  timerId = setInterval(() => {
+    const currentDate = new Date();
+    const ms = selectedDate.getTime() - currentDate.getTime();
+    if (selectedDate.getTime() <= currentDate.getTime()){
+      clearInterval(timerId);
+      return;
+    }
+    const timeLeft = convertMs(ms);
+    daysEl.textContent = addLeadingZero(timeLeft.days);
+    hoursEl.textContent = addLeadingZero(timeLeft.hours);
+    minutesEl.textContent = addLeadingZero(timeLeft.minutes);
+    secEl.textContent = addLeadingZero(timeLeft.seconds);
+  }, 1000)
+}
+
+startBtnEl.addEventListener('click', handlerOnStart);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0')
+}
